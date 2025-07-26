761adfa601fcdaf0db6575670aa97fe15163b14b
_ZiolKen <0022446688d@gmail.com>
2025-07-26 13:06:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -423,7 +423,9 @@ client.login(process.env.TOKEN);
 
 
 const express = require('express');
+const cors = require("cors");
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
