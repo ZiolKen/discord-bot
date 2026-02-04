00b96ff3a6dc4ac1a6efda02ebf37c08d11de436
_ZiolKen <0022446688d@gmail.com>
2026-02-04 11:11:31 +0700

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,32 +1,14 @@
-import pg from "pg";
-const { Pool } = pg;
+const { Pool } = require("pg");
 
 function buildSSL() {
   const caRaw = process.env.AIVEN_CA_CERT;
-
-  if (!caRaw) {
-    return { rejectUnauthorized: true };
-  }
-
-  return {
-    ca: caRaw.replace(/\\n/g, "\n"),
-    rejectUnauthorized: true,
-  };
+  if (!caRaw) return { rejectUnauthorized: true };
+  return { ca: caRaw.replace(/\\n/g, "\n"), rejectUnauthorized: true };
 }
 
-export const pool = new Pool({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: buildSSL(),
-
-  max: 10,
-  idleTimeoutMillis: 30_000,
-  connectionTimeoutMillis: 10_000,
 });
 
-export async function query(text, params) {
-  return pool.query(text, params);
-}
-
-export async function closeDb() {
-  await pool.end();
-}
+module.exports = { pool };
